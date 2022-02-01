0d8fae4ccdbc3d3a4663af9f8f14657646acbdcc
Hamish Meikle <hwhmeikle@gmail.com>
2022-02-01 15:15:35 +1300

    fix: improve accessibility of address input

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -6,13 +6,14 @@ export const Hero = () => {
       <div className="flex flex-col gap-6 items-center pt-60">
         <h1 className="text-4xl">Rate a Rental</h1>
         <p className="text-xl">Discover and review rental properties</p>
-        <div className="flex items-center h-12 w-96 border-2 rounded-md bg-white">
-          <div className="px-3">
-            <HiSearch color="#a8a29e" className="w-6 h-6" />
-          </div>
+        <div className="relative flex items-center h-12 w-96 border-2 rounded-md bg-white">
+          <label htmlFor="address" className="absolute px-3">
+            <HiSearch aria-hidden color="#a8a29e" className="w-6 h-6" />
+            <span className="sr-only">Search for an address</span>
+          </label>
           <input
             id="address"
-            className="h-full w-full outline-0 focus:ring ring-blue-500"
+            className="h-full w-full pl-12 rounded-md outline-0 focus:ring ring-blue-500"
           />
         </div>
       </div>
