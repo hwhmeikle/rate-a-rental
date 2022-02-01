7c7755f31e2d71cca589d142bdb9bc9b36613ebf
Hamish Meikle <hwhmeikle@gmail.com>
2022-02-01 15:08:40 +1300

    Add a tagline

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,18 +2,19 @@ import { HiSearch } from "react-icons/hi";
 
 export const Hero = () => {
   return (
-    <div className="flex flex-col gap-8 justify-center items-center h-128 bg-indigo-100">
-      <label htmlFor="address">
-        <h1 className="text-4xl">What is your address?</h1>
-      </label>
-      <div className="flex items-center h-12 w-96 border-2 rounded-md bg-white">
-        <div className="px-3">
-          <HiSearch color="#a8a29e" className="w-6 h-6" />
+    <div className="h-screen bg-indigo-100">
+      <div className="flex flex-col gap-6 items-center pt-60">
+        <h1 className="text-4xl">Rate a Rental</h1>
+        <p className="text-xl">Discover and review rental properties</p>
+        <div className="flex items-center h-12 w-96 border-2 rounded-md bg-white">
+          <div className="px-3">
+            <HiSearch color="#a8a29e" className="w-6 h-6" />
+          </div>
+          <input
+            id="address"
+            className="h-full w-full outline-0 focus:ring ring-blue-500"
+          />
         </div>
-        <input
-          id="address"
-          className="h-full w-full outline-0 focus:ring ring-blue-500"
-        />
       </div>
     </div>
   );
